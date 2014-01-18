5a35ff5036ef81fb764ecafc2905e993cadbdf4f
shuhei kinukawa <kinushu@gmail.com>
2014-01-19 01:38:21 +0900

    Add sample using new function

---
+++ examples/set_def_out_dev.rb
@@ -0,0 +1,22 @@
+require "coreaudio"
+
+# 入力文字列を名称に含むオーディオデバイスをデフォルト出力として設定
+def set_interface name
+	if !name
+		puts 'please enter audio output interface name.'
+		return -1
+	end
+
+	devs = CoreAudio::devices
+
+	tgt = devs.find{|dev| dev.name.index(name)}
+	if !tgt
+		p 'no match interface' 
+		return -1
+	end
+
+	tgt.set_default_output_device
+	p "select default output audio interface #{tgt.name}"
+end
+
+set_interface ARGV[0]

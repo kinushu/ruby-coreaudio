ed3252fa55c710a52be1532c1d2b0c75793f6dda
shuhei kinukawa <kinushu@gmail.com>
2014-01-19 22:56:25 +0900

    Fix var name

--- examples/set_def_out_dev.rb
+++ examples/set_def_out_dev.rb
@@ -1,21 +1,21 @@
 require "coreaudio"
 
-# 入力文字列を名称に含むオーディオデバイスをデフォルト出力として設定
+# Select option name device as default output device 
 def set_interface(name)
 	if !name
 		puts 'please enter audio output interface name.'
 		return -1
 	end
 
-	devs = CoreAudio::devices
+	devs = CoreAudio.devices
 
 	tgt = devs.find{|dev| dev.name.index(name)}
 	if !tgt
-		p 'no match interface #{tgt.name}' 
+		p "no match interface #{name}"
 		return -1
 	end
 
-	CoreAudio::set_default_output_device(tgt)
+	CoreAudio.set_default_output_device(tgt)
 	p "select default output audio interface #{tgt.name}"
 end
 
